d7b5f4d413344a116ba028b2533fc1831fe7e035
XDean <373216024@qq.com>
2017-10-18 08:11:13 +0800

    update CodecovUpdater

--- src/test/java/xdean/reflect/getter/CodecovUpdater.java
+++ src/test/java/xdean/reflect/getter/CodecovUpdater.java
@@ -1,14 +1,9 @@
 package xdean.reflect.getter;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import xdean.jex.internal.codecov.CodecovIgnoreHandler;
 
 public class CodecovUpdater {
   public static void main(String[] args) {
-    Path codecov = Paths.get("codecov.yml");
-    Path path = Paths.get("src", "main", "java");
-    CodecovIgnoreHandler.updateCodecovIgnore(codecov, path);
+    CodecovIgnoreHandler.updateCodecovIgnore();
   }
 }
