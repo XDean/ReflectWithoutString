e4709c0572f6a3b29493aba0c5fd14298ea42668
XDean <373216024@qq.com>
2018-04-20 14:29:54 +0800

    import entrance class ReflectWithoutString

---
+++ src/main/java/xdean/reflect/getter/ReflectWithoutString.java
@@ -0,0 +1,33 @@
+package xdean.reflect.getter;
+
+import xdean.reflect.getter.impl.ProxyMethodGetter;
+import xdean.reflect.getter.impl.UnsafeFieldGetter;
+
+/**
+ * Entrance utility class to get {@link PropertyGetter}, {@link FieldGetter} or {@link MethodGetter}
+ * 
+ * @author Dean Xu (XDean@github.com)
+ *
+ */
+public interface ReflectWithoutString {
+  /**
+   * Get a default {@link PropertyGetter}
+   */
+  static <T> PropertyGetter<T> propertyGetter(Class<T> clz) {
+    return new UnsafeFieldGetter<>(clz);
+  }
+
+  /**
+   * Get a default {@link FieldPropGetter}
+   */
+  static <T> FieldPropGetter<T> fieldGetter(Class<T> clz) {
+    return new UnsafeFieldGetter<>(clz);
+  }
+
+  /**
+   * Get a default {@link MethodGetter}
+   */
+  static <T> MethodGetter<T> methodGetter(Class<T> clz) {
+    return new ProxyMethodGetter<>(clz);
+  }
+}
